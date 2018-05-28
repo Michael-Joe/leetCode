0e681c7f69e22a929983d76eefcf662a16f22e51
michael_joe <michael_joe1023@outlook.com>
2018-05-28 23:43:21 +0800

    nineteenth

---
+++ deleteNode/src/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Date:  2018.05.28
+ * Url:   https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/41/
+ * Title: 删除链表的结点
+ *
+ */
+
+public class Main {
+}

6a9347b7e4c3eb50ef0c077970a6d399e1e671af
michael_joe <michael_joe1023@outlook.com>
2018-06-04 23:51:25 +0800

    mtwenty-second

--- mergeTwoLists/src/Main.java
+++ mergeTwoLists/src/Main.java
@@ -1,15 +1,24 @@
+/*
+ * Date:  2018.06.04
+ * Url:   https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/44/
+ * Title: 合并两个有序链表
+ *
+ */
+
 public class Main {
     public static void main(String[] args) {
         Solution handler = new Solution();
-        ListNode l1 = new ListNode(4);
-        l1 = insert(l1, 2);
-        l1 = insert(l1, 1);
-
+//        ListNode l1 = new ListNode(4);
+//        l1 = insert(l1, 2);
+//        l1 = insert(l1, 1);
+ListNode l1=null;
         ListNode l2 = new ListNode(4);
+        l2 = insert(l2, 3);
         l2 = insert(l2, 2);
         l2 = insert(l2, 1);
 
-        ListNode tmp = handler.mergeTwoLists(l1, l2);
+        ListNode tmp = new ListNode(0);
+        tmp=handler.mergeTwoLists(l1,l2);
 
         while (tmp != null) {
             System.out.print(tmp.val + ",");
--- mergeTwoLists/src/Solution.java
+++ mergeTwoLists/src/Solution.java
@@ -1,101 +1,75 @@
-
+/*
+ * 注：比较节点，尾插法；
+ */
 
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode list = null;
-        ListNode temp = null;
-        ListNode p = null;
-        ListNode tail = null;
         ListNode list1 = l1;
         ListNode list2 = l2;
+        ListNode list = null;
 
         while (list1 != null || list2 != null) {
-            if (list1.next == null) {
-
-                p = list;
-                if (p == null) {
-                    p=list2;
-                } else {
-                    while (p.next != null) {
-                        p = p.next;
+            if (list1 == null && list2 != null) {
+                if (list != null) {
+                    ListNode tmp = list;
+                    while (tmp.next != null) {
+                        tmp = tmp.next;
                     }
-                    p.next = list2.next;
-                }
-
-                break;
-            }
-
-            if (list2.next == null) {
-
-                p = list;
-                if (p == null) {
-                    p=list1;
+                    tmp.next = list2;
                 } else {
-                    while (p.next != null) {
-                        p = p.next;
-                    }
-                    p.next = list1.next;
+                    list = list2;
                 }
-
-                break;
-            }
-
-            if (list1.val > list2.val) {
-                p = list;
-                if (p == null) {
-                    temp.next = p;
-                    p = temp;
+                return list;
+            } else if (list1 != null && list2 == null) {
+                if (list != null) {
+                    ListNode tmp = list;
+                    while (tmp.next != null) {
+                        tmp = tmp.next;
+                    }
+                    tmp.next = list1;
                 } else {
-                    while (p.next != null) {
-                        p = p.next;
-                    }
-                    p.next = temp;
+                    list = list1;
                 }
-
-                list2 = list2.next;
-            } else if (list1.val < list2.val) {
-                temp = new ListNode(list1.val);
-                p = list;
-                if (p == null) {
-                    temp.next = p;
-                    p = temp;
-                } else {
-                    while (p.next != null) {
-                        p = p.next;
-                    }
-                    p.next = temp;
-                }
-
-                list1 = list1.next;
+                return list;
+            } else if (list1 == null && list2 == null) {
+                return list;
             } else {
-                temp = new ListNode(list1.val);
-                p = list;
-                if (p == null) {
-                    temp.next = p;
-                    p = temp;
+                if (list1.val > list2.val) {
+                    list = tailInsert(list, list2);
+                    list2 = list2.next;
+                } else if (list1.val < list2.val) {
+                    list = tailInsert(list, list1);
+                    list1 = list1.next;
                 } else {
-                    while (p.next != null) {
-                        p = p.next;
-                    }
-                    p.next = temp;
+                    list = tailInsert(list, list1);
+                    list1 = list1.next;
+                    list = tailInsert(list, list2);
+                    list2 = list2.next;
                 }
-
-                temp = new ListNode(list2.val);
-                p = list;
-                if (p == null) {
-                    temp.next = p;
-                    p = temp;
-                } else {
-                    while (p.next != null) {
-                        p = p.next;
-                    }
-                    p.next = temp;
-                }
-
-                list1 = list1.next;
-                list2 = list2.next;
             }
         }
+
+        return list;
+    }
+
+    public ListNode tailInsert(ListNode list, ListNode data) {
+        ListNode tmp = list;
+        ListNode node = new ListNode(0);
+
+        node.next = null;
+        node.val = data.val;
+
+        if (list == null) {
+            list = node;
+            return list;
+        }
+
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        node.next = tmp.next;
+        tmp.next = node;
+
         return list;
     }
 }
